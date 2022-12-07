4bfb39fd93c87579e44fa99c5794cc95e6751188
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2022-12-07 16:57:02 +0300

    added a functionality to allow users to add task to the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", addTask);
+
+function addTask(){
+  const li = document.createElement('li')
+  const toDo = document.querySelector('#new-task-description')
+  const ul = document.querySelector('#tasks')
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+  ul.appendChild(li);
+  li.innerHTML=toDo.value
+  })
+}
